31f164251aa2e504d7e04173a9bbed5f4dd774f6
mgautam <gautimeena456@gmail.com>
2019-04-09 18:45:52 +0530

    Update

--- Quiz/problem2.c
+++ Quiz/problem2.c
@@ -1,3 +1,17 @@
+// Write a c function, which takes two integer arrays as arguments.
+// From these two arrays find a pair of values (one from each array) that 
+// you can swap to give the two arrays the same sum. Print these two values 
+// separated by a comma to output on a new line. If no such pair exists print "Not Possible".
+// (You could have more than one valid pair for given input, in such cases printing any one valid pair is acceptable)
+// Example : Input : {4,1,2,1,1,2} and {3,6,3,3}
+// 		     OutPut : {1,3}
+
+
+// Author : Gautam Meena
+// 15 March 2019
+// Week : 2, Problem : 2
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,7 +36,7 @@ int main(void)
 
 }
 
-
+// Input array
 void get_input(int arr[], int size)
 {
 	for (int i = 0; i < size; i++)
@@ -32,10 +46,12 @@ void get_input(int arr[], int size)
 }
 
 
+// Finds the required pair if present
 void foo(int arr1[], int size1, int arr2[], int size2)
 {
 	int sum1 = 0, sum2 = 0;
 
+	// Calculates the sum of elements of array
 	for (int i = 0; i < size1; i++){
 		sum1 += arr1[i];
 	}
@@ -44,18 +60,20 @@ void foo(int arr1[], int size1, int arr2[], int size2)
 		sum2 += arr2[i];
 	}
 
+
+	// If pair is not possible
 	if ((sum1 + sum2)%2 != 0)
 	{
 		printf("Not Possible");
 		return;
 	}
 
-	int diff = abs(sum1 - (sum1 + sum2)/2);
+	int diff = (sum1 - (sum1 + sum2)/2);
 	for (int i = 0; i < size1; i++)
 	{
 		for (int j = 0; j < size2; j++)
 		{
-			if (abs(arr1[i] - arr2[j]) == diff)
+			if ((arr1[i] - arr2[j]) == diff)
 			{
 				printf("%d,%d", arr1[i], arr2[j]);
 				return;
